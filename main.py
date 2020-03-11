1257d216a152d655692195bfc28d7361f4be8a07
usk39 <60925409+usk39@users.noreply.github.com>
2020-03-11 19:08:43 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 import os
 
-import quickstart
+import quickstart  # gcapi.py
 
 from linebot import (
    LineBotApi, WebhookHandler
@@ -16,31 +16,33 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# 環境変数からLINE Access Tokenを設定
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-# 環境変数からLINE Channel Secretを設定
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+# 環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+@app.route("/")
+def hello_world():
+   return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+   # handle webhook body
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       abort(400)
 
-    return 'OK'
+   return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
