88b4d939e07dbecde78322116c6efd49dd028e44
usk39 <60925409+usk39@users.noreply.github.com>
2020-03-11 18:08:46 +0900

    Add files via upload

--- quickstart.py
+++ quickstart.py
@@ -8,12 +8,9 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -34,20 +31,88 @@ def main():
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
+    return service
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
+def read(date):
+   service = main()
+   results = date + 'の予定'
+   dateMax = date + 'T23:59:00+09:00'
+   date += 'T00:00:00+09:00'
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+   events_result = service.events().list(calendarId='73t3ahvdjq4vh3dujgosa13868@group.calendar.google.com', timeMin=date, timeMax=dateMax,
+                                       maxResults=10, singleEvents=True, orderBy='startTime').execute()
+   events = events_result.get('items', [])
 
-if __name__ == '__main__':
-    main()
+   if not events:
+       results += 'は見つかりませんでした。'
+       return results
+   result = []
+   for event in events:
+       summary = event.get('summary', '未指定') #イベントタイトル
+       start = event['start'].get('dateTime', '終日')
+       end = ''
+       if start != '終日':
+           start = datetime.strptime(start, '%Y-%m-%dT%H:%M:%S%z')
+           start = datetime.strftime(start, '%H:%M')
+           end = event['end'].get('dateTime')
+           end = datetime.strptime(end, '%Y-%m-%dT%H:%M:%S%z')
+           end = datetime.strftime(end, '%H:%M')
+       if start == end or start == '終日':
+           start_end = start
+       else:
+           start_end = start + '-' + end
+       location = event.get('location', '未指定')
+       result.append('予定:'+summary + ' 時間:' + start_end + ' 場所:' + location)
+   results += '\n' + '\n'.join(result)
+   return results
+
+def extract_words(str):
+   m = re.match('(.+)\n(.*)\n(\d{8})\n(\d{4})\n(\d{4})', str)
+   if m is None:
+       return
+   mg = list(m.groups())
+   mg[3] = mg[2] + " " + mg[3]
+   mg[4] = mg[2] + " " + mg[4]
+   mg.pop(2)
+   return mg
+
+def write(title, location, start, end):
+   if start[4:] == '0000' and end[4:] == '2400':
+       start = datetime.strptime(start[:8], '%Y%m%d')
+       start = datetime.strftime(start, '%Y-%m-%d')
+       end = datetime.strptime(end[:8], '%Y%m%d')
+       end = datetime.strftime(end, '%Y-%m-%d')
+       event = {
+           'summary': title,
+           'location': location,
+           'start': {
+               'date': start
+           },
+           'end': {
+               'date': end
+           }
+       }
+   else:
+       start = datetime.strptime(start, '%Y%m%d %H%M')
+       start = datetime.strftime(start, '%Y-%m-%dT%H:%M:%S+09:00')
+       try:
+           end = datetime.strptime(end, '%Y%m%d %H%M')
+           end = datetime.strftime(end, '%Y-%m-%dT%H:%M:%S+09:00')
+       except ValueError: #24時の場合は翌日の0時に変換
+           end = datetime.strptime(end[:8], '%Y%m%d')
+           end = end + timedelta(days=1)
+           end = datetime.strftime(end, '%Y-%m-%dT00:00:00+09:00')
+       event = {
+           'summary': title,
+           'location': location,
+           'start': {
+               'dateTime': start
+           },
+           'end': {
+               'dateTime': end
+           }
+       }
+   service = main()
+   event = service.events().insert(calendarId='73t3ahvdjq4vh3dujgosa13868@group.calendar.google.com', body=event).execute()
+   msg = event.get('htmlLink')
+   return msg
