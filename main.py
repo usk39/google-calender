a28b950ade948e24aa84f0dc1dd209bdaad238e3
usk39 <60925409+usk39@users.noreply.github.com>
2020-03-15 23:11:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 import os
 
-import quickstart
+import python-main
 
 from linebot import (
    LineBotApi, WebhookHandler
@@ -47,7 +47,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
    push_text = event.message.text
-   text = quickstart.extract_words(push_text)
+   text = python-main.extract_words(push_text)
    if text is None:
        text_message = TextSendMessage(text="Googleカレンダーの予定を知りたいですか？それとも追加したいですか？",
                                       quick_reply=QuickReply(items=[
@@ -58,14 +58,14 @@ def handle_message(event):
                                       ]))
        line_bot_api.reply_message(event.reply_token, text_message)
    else:
-       htmllink = quickstart.write(*text)
+       htmllink = python-main.write(*text)
        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=htmllink))
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
    if event.postback.data == "read":
        date = event.postback.params['date']
-       msg = quickstart.read(date)
+       msg = python-main.read(date)
        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
    else:
        event.postback.data == "write"
